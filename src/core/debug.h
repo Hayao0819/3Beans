088e6666f00e0c0291288a9019d85b38cecf6828
hayao <shun819.mail@gmail.com>
2026-07-28 05:51:42 +0900

    Add the license header to the files this fork adds

--- src/core/debug.h
+++ src/core/debug.h
@@ -1,3 +1,22 @@
+/*
+    Copyright 2026 Hayao0819
+
+    This file is part of 3Beans.
+
+    3Beans is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    3Beans is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with 3Beans. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 // debug.h - inspection of a running core, with no dependency on how it is presented.
 // The MCP server (desktop/debugger.h) and the debugger window (desktop/debug_dialog.h)
 // are both thin wrappers around this.
--- src/desktop/debugger.h
+++ src/desktop/debugger.h
@@ -1,3 +1,22 @@
+/*
+    Copyright 2026 Hayao0819
+
+    This file is part of 3Beans.
+
+    3Beans is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    3Beans is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with 3Beans. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 // debugger.h - MCP debug server exposing emulator-native operations (run control,
 // key input, memory/register dumps, MMU-translated reads) over local TCP.
 // Enabled when B3_MCP_PORT is set; connect with `socat STDIO TCP:127.0.0.1:<port>`.
